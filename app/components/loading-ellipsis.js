e72be279d89b1de42d181a6660a33c98ee034d9b
Mike Hoffert <mike.hoffert@gmail.com>
2017-02-27 13:57:26 -0700

    Loading ellipsis

--- app/components/loading-ellipsis.js
+++ app/components/loading-ellipsis.js
@@ -1,9 +1,32 @@
 import React from "react";
 
 const Ellipsis = React.createClass({
+  interval: null,
+
+  getInitialState() {
+    return({
+      count: 0
+    });
+  },
+
+  componentDidMount() {
+    this.interval = setInterval(this.updateDots, 500);
+  },
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  },
+
+  updateDots() {
+    let count = (this.state.count >= 3)? 0 : this.state.count + 1;
+    this.setState({count: count});
+  },
+
   render() {
+    let dots = ".".repeat(this.state.count);
+
     return(
-      <div></div>
+      <div>{this.props.children || "Loading"}{dots}</div>
     );
   }
 });
